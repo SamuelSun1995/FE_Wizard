99de580e92bdcb51a53a9e5724a1218f9d1d5b84
sunzhengxiang <sunzhengxiang@xiaomi.com>
2023-07-13 21:01:38 +0800

    navigation完成

--- app/src/main/java/com/yifan/fewizard/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/yifan/fewizard/ui/activity/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.yifan.fewizard.ui.activity.main
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.yifan.fewizard.R
 import com.yifan.fewizard.base.BaseActivity
 import com.yifan.fewizard.databinding.ActivityMainBinding
@@ -13,11 +16,50 @@ import com.yifan.fewizard.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
+    private val _tag = "MainActivity"
     override fun init() {
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_mine))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        setupWithNavController(mDataBinding.bottomNav, navController)
+        onClickInit()
+    }
+
+    private fun onClickInit() {
+        mDataBinding.navHome.setOnClickListener {
+            try {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.nav_home)
+            } catch (e: IllegalArgumentException) {
+                Log.e(_tag, "findNavController home IllegalArgumentException...")
+            }
+        }
+
+        mDataBinding.navMine.setOnClickListener {
+            try {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.nav_mine)
+            } catch (e: IllegalArgumentException) {
+                Log.e(_tag, "findNavController mine IllegalArgumentException...")
+            }
+        }
+
+        mDataBinding.add.setOnClickListener {
+            //弹出对话框
+            val builder = AlertDialog.Builder(this).setTitle("记录油耗").setIcon(R.mipmap.fuel)
+            val diaLayout =
+                layoutInflater.inflate(R.layout.dialog_add_fuel_layout, null) as LinearLayout
+            builder.setView(diaLayout)
+            builder.setPositiveButton("确定") { _: DialogInterface, _: Int ->
+                val bundle = Bundle()
+                bundle.putFloat(
+                    "mileage",
+                    diaLayout.findViewById<EditText>(R.id.ed_mileage).text.trim().toString().toFloat()
+                )
+                bundle.putFloat(
+                    "refuel",
+                    diaLayout.findViewById<EditText>(R.id.ed_refuel).text.trim().toString().toFloat()
+                )
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.nav_home, bundle)
+            }
+            builder.setNegativeButton("取消") { _: DialogInterface, _: Int ->
+            }
+            builder.create().show()
+        }
     }
 
     override fun getLayoutResId(): Int {
--- app/src/main/java/com/yifan/fewizard/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/yifan/fewizard/ui/fragment/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.yifan.fewizard.ui.fragment.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.yifan.fewizard.R
@@ -7,10 +8,17 @@ import com.yifan.fewizard.base.BaseFragment
 import com.yifan.fewizard.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+
+    private val _tag = "HomeFragment"
     override fun init() {
-//        handleEventDelay({
-//            findNavController().navigate(R.id.action_homeFragment_to_mineFragment)
-//        }, 5000)
+        val bundle = arguments
+        try {
+            val lastFuel = bundle?.getFloat("refuel")?.div(bundle.getFloat("mileage"))?.times(100)
+            Log.d(_tag, "实际油耗:"+ lastFuel.toString())
+            mDataBinding.tvLastFuelFact.text = lastFuel.toString()
+        }catch (_:ArithmeticException){
+
+        }
     }
 
     override fun getLayoutResId(): Int {
